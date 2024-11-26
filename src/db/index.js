8054ec4d6ad765ddb44aa7559804b53c61c53a2c
bhargavpuriGoswami <bjg1799@gmail.com>
2024-11-26 12:24:21 -0500

    gitignore is working

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js";
+
+
+const connectDB = async ()=>{
+    try{
+        console.log(process.env.MONGODB_URL);
+        const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`\n MongoDB connected ! DB host: ${connectionInstance.connection.host}`);
+    }
+    catch(error){
+        console.log("Database connection error", error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import {app} from "./app.js"
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path:'./.env'
+})
+
+const PORT = process.env.PORT || 3000;
+
+
+connectDB()
+.then(()=>{
+    app.listen(PORT,()=>{
+
+        console.log(`Server is running on 127.0.0.1/${PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log('MongoDB connection error', err);
+})
+
+
